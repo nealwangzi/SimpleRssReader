9cd183dbb04d5cdc69205c4809a9d7cfc3643940
neal <caoyahua@cloudoc.cn>
2017-06-29 10:46:06 +0800

    Initial Commit

---
+++ simpleRssReader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  simpleRssReader
+//
+//  Created by neal on 2017/6/29.
+//  Copyright © 2017年 Cloudoc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
